68ee4c1971ec57576ef2c3c1ed4b69c849d495ef
William Oldham <wegg7250@gmail.com>
2019-06-29 17:48:44 +0100

    Improve challenge 6 using rprompt

--- challenge-6/src/main.rs
+++ challenge-6/src/main.rs
@@ -1,11 +1,9 @@
-use std::io::{stdin, stdout, Read, Write};
+use rprompt::prompt_reply_stdout;
 use std::time::SystemTime;
 
 fn main() {
-    print!("Press enter when you think 10 seconds has elapsed!");
-    stdout().flush().unwrap();
     let start = SystemTime::now();
-    stdin().read(&mut [0]).unwrap();
+    prompt_reply_stdout("Press enter when you think 10 seconds has elapsed!").expect("Could not read from input!");
     let time_taken = (SystemTime::now().duration_since(start).unwrap().as_millis()) / 1000;
     print!("You pressed enter after {} seconds! {}", time_taken, if time_taken as u32 == 10 {"You got the achievement!"} else {"You didn't quite get it..."});
 }
